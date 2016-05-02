f9885458fa48bb33beac411b0431057ff2a3b178
Arlen Burton and Darius Capizzi <darius@>
2016-05-02 14:13:20 -0700

    database exploration

--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -1,60 +1,116 @@
 import java.time.LocalDateTime;
 import java.util.ArrayList;
+import java.util.List;
+import org.sql2o.*;
 
 public class Task {
-  private String mDescription;
-  private boolean mCompleted;
-  private LocalDateTime mCreatedAt;
-  private static ArrayList<Task> instantFood = new ArrayList<Task>();
-  private int mId;
+  // private String mDescription;
+  // private boolean mCompleted;
+  // private LocalDateTime mCreatedAt;
+  // private static ArrayList<Task> instantFood = new ArrayList<Task>();
+  // private int mId;
+
+
+//server-database
+  private int id;
+  private String description;
+
 
   public Task(String description) {
-    mDescription = description;
-    mCompleted = false;
-    mCreatedAt = LocalDateTime.now();
-    mId = instantFood.size();
-    instantFood.add(this);
-  }
-
-
-  public static Task find(int id) {
-    try {
-      return instantFood.get(id);
-    } catch (IndexOutOfBoundsException e) {
-      System.out.println(e);
-      return null;
-    }
+    this.description = description;
   }
 
   public String getDescription() {
-    return mDescription;
-  }
-
-  public boolean isCompleted() {
-    return mCompleted;
-  }
-
-  public void completedTask(){
-    if(mCompleted){
-      mCompleted = false;
-    } else {
-      mCompleted = true;
-    }
-  }
-
-  public LocalDateTime getCreatedAt() {
-    return mCreatedAt;
-  }
-
-  public static ArrayList<Task> all() {
-    return instantFood;
-  }
-
-  public static void clear() {
-    instantFood.clear();
+    return description;
   }
 
   public int getId() {
-    return mId;
+    return id;
   }
+
+  public static List<Task> all(){
+    try(Connection con = DB.sql2o.open()) {
+      String sql = "SELECT * FROM tasks;";
+      return con.createQuery(sql).executeAndFetch(Task.class);
+    }
+  }
+
+  public static Task find(int id) {
+    try(Connection con = DB.sql2o.open()) {
+      String sql = "SELECT * FROM tasks where id=:id";
+      Task task = con.createQuery(sql).addParameter("id", id).executeAndFetchFirst(Task.class);
+      return task;
+    }
+  }
+
+  @Override
+  public boolean equals(Object otherTask) {
+    if (!(otherTask instanceof Task)) {
+      return false;
+    } else {
+      Task newTask = (Task) otherTask;
+      return this.getDescription().equals(newTask.getDescription());
+    }
+  }
+
+  public void save() {
+    try(Connection con = DB.sql2o.open()){
+      String sql = "INSERT INTO tasks (description) VALUES (:description)";
+      this.id = (int) con.createQuery(sql, true).addParameter("description", description).executeUpdate().getKey();
+    }
+  }
+
+
+// // server-client
+//   public Task(String description) {
+//     mDescription = description;
+//     mCompleted = false;
+//     mCreatedAt = LocalDateTime.now();
+//     mId = instantFood.size();
+//     instantFood.add(this);
+//   }
+//
+//   public static Task find(int id) {
+//     try {
+//       return instantFood.get(id);
+//     } catch (IndexOutOfBoundsException e) {
+//       System.out.println(e);
+//       return null;
+//     }
+//   }
+//
+//   public String getDescription() {
+//     return mDescription;
+//   }
+//
+//   public boolean isCompleted() {
+//     return mCompleted;
+//   }
+//
+//   public void completedTask(){
+//     if(mCompleted){
+//       mCompleted = false;
+//     } else {
+//       mCompleted = true;
+//     }
+//   }
+//
+//   public LocalDateTime getCreatedAt() {
+//     return mCreatedAt;
+//   }
+//
+//   public static ArrayList<Task> all() {
+//     return instantFood;
+//   }
+//
+//   public static void clear() {
+//     instantFood.clear();
+//   }
+//
+//   public int getId() {
+//     return mId;
+//   }
+
+
+
 }
--- src/test/java/TaskTest.java
+++ src/test/java/TaskTest.java
@@ -1,3 +1,4 @@
+import org.sql2o.*;
 import org.junit.*;
 import static org.junit.Assert.*;
 import java.time.LocalDateTime;
@@ -5,16 +6,55 @@ import java.time.LocalDateTime;
 
 public class TaskTest {
 
-  // @Test
-  // public void Task_instantiatesCorrectly_true() {
-  //   Task myTask = new Task("Mow the lawn");
-  //   assertEquals(true, myTask instanceof Task);
-  // }
-  // @Test
-  // public void Task_instantiatesWithDescription_String() {
-  //   Task myTask = new Task("Mow the lawn");
-  //   assertEquals("Mow the lawn", myTask.getDescription());
-  // }
+  @Before
+  public void setUp() {
+    DB.sql2o = new Sql2o("jdbc:postgresql://localhost:5432/to_do", null, null);
+  }
+
+
+  @After
+  public void tearDown() {
+    try(Connection con = DB.sql2o.open()) {
+      String deleteTasksQuery = "DELETE FROM tasks *;";
+      // String deleteCategoriesQuery = "DELETE FROM categories *;";
+      con.createQuery(deleteTasksQuery).executeUpdate();
+      // con.createQuery(deleteCategoriesQuery).executeUpdate();
+    }
+  }
+
+  @Test
+  public void save_assignsIdToObject() {
+    Task myTask = new Task("Mow the lawn");
+    myTask.save();
+    Task savedTask = Task.all().get(0);
+    assertEquals(myTask.getId(), savedTask.getId());
+  }
+
+  @Test
+  public void all_emptyAtFirst() {
+    assertEquals(0, Task.all().size());
+  }
+
+  @Test
+  public void equals_returnsTrueIfDescriptionsAretheSame() {
+    Task firstTask = new Task("Mow the lawn");
+    Task secondTask = new Task("Mow the lawn");
+    assertTrue(firstTask.equals(secondTask));
+  }
+
+  @Test
+  public void Task_instantiatesCorrectly_true() {
+    Task myTask = new Task("Mow the lawn");
+    assertEquals(true, myTask instanceof Task);
+  }
+  @Test
+  public void getDescription_instantiatesWithDescription_String() {
+    Task myTask = new Task("Mow the lawn");
+    assertEquals("Mow the lawn", myTask.getDescription());
+  }
+
+
+
   //
   // @Test
   // public void isCompleted_isFalseAfterInstantiation_false() {
@@ -28,35 +68,35 @@ public class TaskTest {
   //   Task myTask = new Task("Mow the lawn");
   //   assertEquals(LocalDateTime.now().getDayOfWeek(), myTask.getCreatedAt().getDayOfWeek());
   // }
-
-  @Test
-  public void all_returnsAllInstancesOfTask_true() {
-    Task firstTask = new Task("Mow the lawn");
-    Task secondTask = new Task("Buy groceries");
-    // System.out.println(firstTask.all());
-    assertTrue(secondTask.all().contains(firstTask));
-    assertTrue(Task.all().contains(secondTask));
-  }
-
-  @Test
-  public void clear_emptiesAllTasksFromArrayList_0() {
-    Task myTask = new Task("Mow the lawn");
-    Task.clear();
-    assertEquals(Task.all().size(), 0);
-  }
-
-  @Test
-  public void getId_tasksInstantiateWithAnID_1() {
-    Task.clear();  // THIS TEST WILL FAIL WITHOUT THIS LINE!
-    Task myTask = new Task("Mow the lawn");
-    assertEquals(0, myTask.getId());
-  }
-
-  @Test
-  public void find_returnsTaskWithSameId_secondTask() {
-    Task.clear();
-    Task firstTask = new Task("Mow the lawn");
-    Task secondTask = new Task("Buy groceries");
-    assertEquals(Task.find(secondTask.getId()), secondTask);
-  }
+  //
+  // @Test
+  // public void all_returnsAllInstancesOfTask_true() {
+  //   Task firstTask = new Task("Mow the lawn");
+  //   Task secondTask = new Task("Buy groceries");
+  //   // System.out.println(firstTask.all());
+  //   assertTrue(secondTask.all().contains(firstTask));
+  //   assertTrue(Task.all().contains(secondTask));
+  // }
+  //
+  // @Test
+  // public void clear_emptiesAllTasksFromArrayList_0() {
+  //   Task myTask = new Task("Mow the lawn");
+  //   Task.clear();
+  //   assertEquals(Task.all().size(), 0);
+  // }
+  //
+  // @Test
+  // public void getId_tasksInstantiateWithAnID_1() {
+  //   Task.clear();  // THIS TEST WILL FAIL WITHOUT THIS LINE!
+  //   Task myTask = new Task("Mow the lawn");
+  //   assertEquals(0, myTask.getId());
+  // }
+  //
+  // @Test
+  // public void find_returnsTaskWithSameId_secondTask() {
+  //   Task.clear();
+  //   Task firstTask = new Task("Mow the lawn");
+  //   Task secondTask = new Task("Buy groceries");
+  //   assertEquals(Task.find(secondTask.getId()), secondTask);
+  // }
 }
